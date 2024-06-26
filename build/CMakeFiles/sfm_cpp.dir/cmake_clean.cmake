2ca901522b459e3034e460cd101920baf95d4584
Dev Soni <devsoni44.pp@gmail.com>
2024-06-25 21:39:55 -0400

    Throught cleaning -- added build

---
+++ build/CMakeFiles/sfm_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfm_cpp.dir/src/Main.cpp.o"
+  "CMakeFiles/sfm_cpp.dir/src/Main.cpp.o.d"
+  "sfm_cpp"
+  "sfm_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfm_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sfm_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sfm_cpp.
+# This may be replaced when dependencies are built.
